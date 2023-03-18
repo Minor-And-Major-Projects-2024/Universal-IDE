0b736b6266afd19f3c6627b6e1c63407a44d4a34
mstomar698 <tomarm698@gmail.com>
2023-03-18 22:35:12 +0530

    Bug: cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { Demo } from '@/components/demo/demo';
 import Homepage from '@/components/homepage/homepage';
 
 export default function Home() {
   return (
     <>
-      {/* <Demo /> */}
       <Homepage />
     </>
   );
